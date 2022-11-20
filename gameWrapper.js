a745a14ef792615555f4bf1b65ceb5c95e29714e
Martin Krauskopf <martin.krauskopf@gmail.com>
2022-11-20 10:04:32 +0100

    Set up Prettier

--- gameWrapper.js
+++ gameWrapper.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict'
 
-import { Game } from "./game.js";
+import { Game } from './game.js'
 
 class GameWrapper {
   constructor(gameElement) {
     new p5((p5) => {
-      new Game(p5, 400, 300);
-    }, gameElement);
+      new Game(p5, 400, 300)
+    }, gameElement)
   }
 }
 
-new GameWrapper("game");
+new GameWrapper('game')
--- tools.js
+++ tools.js
@@ -4,8 +4,8 @@ const KeyCodes = {
   D_CODE: 68,
   S_CODE: 83,
   W_CODE: 87,
-};
+}
 
-const Direction = Object.freeze({ LEFT: 1, RIGHT: 2, UP: 3, DOWN: 4, NONE: 5 });
+const Direction = Object.freeze({ LEFT: 1, RIGHT: 2, UP: 3, DOWN: 4, NONE: 5 })
 
-export { KeyCodes, Direction };
+export { KeyCodes, Direction }
